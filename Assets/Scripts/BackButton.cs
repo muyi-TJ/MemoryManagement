235fcb39d7f10fc3d21c0eba9c94e9cb9ce3e9ed
muyi <524453007@qq.com>
2021-06-05 19:09:44 +0800

    删掉了不必要的界面

--- Assets/Scripts/BackButton.cs
+++ Assets/Scripts/BackButton.cs
@@ -9,10 +9,7 @@ public class BackButton : Button
     // Start is called before the first frame update
     void Start()
     {
-        this.onClick.AddListener(delegate ()
-        {
-            SceneManager.LoadScene("Initial");
-        });
+        this.onClick.AddListener(Application.Quit);
     }
 
     // Update is called once per frame
